a2e4786ffbf7bb5695322c861fad9f142e7d8353
Nicolas GRANGER <Nicolas62x@gmail.com>
2021-01-05 21:13:08 +0100

    42

---
+++ Backuper/Backuper.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Backuper
+{
+
+    class Backuper
+    {
+
+        public static string CurrentFolder = AppDomain.CurrentDomain.BaseDirectory.Replace('\\', '/');
+        public static void TitleWrite(string text)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                Console.Title = text;
+            else
+                Console.Write($"\u001B]0;{text}\u0007");
+        }
+        struct BackupData
+        {
+            public string date;
+            public string Size;
+            public string error;
+        }
+        class stringContainer
+        {
+            public string s;
+        }
+
+        static void Main(string[] args)
+        {
+
+            Config config = new Config();
+
+            stringContainer[] status = new stringContainer[config.Querries.Length];
+
+            for (int i = 0; i < config.Querries.Length; i++)
+            {
+                Console.WriteLine("Initializing querry: \n" + config.Querries[i].ToString());
+
+                status[i] = new stringContainer();
+                HandleQuerry(config.Querries[i], status[i]);
+            }
+
+            Thread.Sleep(5000);
+
+            while (true)
+            {
+                Console.Clear();
+                foreach (stringContainer sc in status)
+                {
+                    Console.WriteLine(sc.s);
+                }
+
+
+                Thread.Sleep(2000);
+            }
+
+        }
+
+        static void HandleQuerry(BackupQuerry q, stringContainer status)
+        {
+            Task.Run(() =>
+            {
+
+                q.BackupFolder = q.BackupFolder.Replace('\\', '/');
+                q.FolderToBackup = q.FolderToBackup.Replace('\\', '/');
+
+                for (int i = 0; i < q.Ignore.Length; i++)
+                {
+                    q.Ignore[i] = q.Ignore[i].Replace('\\', '/');
+                }
+
+                int hours = int.Parse(q.date.Split(':')[0]);
+                int minutes = int.Parse(q.date.Split(':')[1]);
+
+                Queue<string> Backups = new Queue<string>();
+
+                DateTime t = DateTime.Today.AddHours(hours).AddMinutes(minutes);
+
+                if (DateTime.Now > t)
+                    t = t.AddDays(1);
+
+                BackupData dats = new BackupData();
+
+                dats.date = "N/A";
+                dats.error = "";
+                dats.Size = "";
+
+
+                status.s = "Next backup of: " + q.FolderToBackup + " At: " + t + " In: " + (t - DateTime.Now).ToString("hh\\:mm\\:ss") + "\nLast: " + dats.date + " " + dats.Size + " " + dats.error + "              ";
+
+
+                while (true)
+                {
+                    if ((t - DateTime.Now).TotalSeconds < 30)
+                    {
+                        MakeBackup(q.FolderToBackup, q.BackupFolder, in Backups, q.Ignore, ref dats);
+                        
+                        Console.WriteLine("Next backup: " + t);
+
+                        if (Backups.Count > 1 && Backups.Count > q.BackupCountToKeep)
+                        {
+                            string p = Backups.Dequeue();
+
+                            if (Directory.Exists(p))
+                            {
+                                Console.WriteLine("Deleting old backup: " + p);
+                                Directory.Delete(p, true);
+                            }
+
+                        }
+
+                        switch (q.Occurences)
+                        {
+                            case BackupOccurences.Hourly:
+
+                                t = t.AddHours(q.OccurenceFactor);
+
+                                break;
+                            case BackupOccurences.Dayly:
+
+                                t = t.AddDays(q.OccurenceFactor);
+
+                                break;
+                            case BackupOccurences.Monthly:
+
+                                t = t.AddDays(q.OccurenceFactor * 30.437);
+
+                                break;
+                            default:
+                                break;
+                        }
+
+
+                    }
+                    else
+                    {
+
+
+                        status.s = "Next backup of: " + q.FolderToBackup + " At: " + t + " In: " + (t - DateTime.Now).ToString("hh\\:mm\\:ss") + "\nLast: " + dats.date + " " + dats.Size + " " + dats.error + "              ";
+                    }
+
+                    Thread.Sleep(1000);
+                }
+
+            });
+
+        }
+
+
+        static void MakeBackup(string from, string to, in Queue<string> Backups, string[] ignore, ref BackupData dats)
+        {
+            if (!from.EndsWith("/"))
+                from = from + "/";
+
+            if (!to.EndsWith("/"))
+                to = to + "/";
+
+            dats.error = "";
+
+            if (!Directory.Exists(from))
+            {
+                dats.error = $"{from} does not exist ({DateTime.Now})";
+                return;
+            }
+
+            if (!Directory.Exists(to))
+            {
+                Directory.CreateDirectory(to);
+            }
+
+            string[] files = Directory.GetFiles(from, "*", SearchOption.AllDirectories);
+
+            Int64 dataLenght = 0;
+
+            List<string> tmp = new List<string>();
+
+            foreach (string ss in files)
+            {
+                string s = ss.Replace('\\', '/');
+                try
+                {
+                    FileInfo fi = new FileInfo(s);
+
+                    bool ok = true;
+
+                    foreach (string ig in ignore)
+                    {
+                        if (ig.EndsWith("/") && s.Contains(from + ig))
+                        {
+                            ok = false;
+                            break;
+                        }
+                        else if (!ig.EndsWith("/") && s.EndsWith(ig))
+                        {
+                            ok = false;
+                            break;
+                        }
+
+                    }
+
+                    if (!ok)
+                        continue;
+
+                    tmp.Add(s);
+                    if (fi.Exists)
+                        dataLenght += fi.Length;
+
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+
+            files = tmp.ToArray();
+
+            dats.Size = (dataLenght / 1048576.0).ToString("F2") + " MiBytes";
+
+            if (!Directory.Exists(to))
+            {
+                Directory.CreateDirectory(to);
+            }
+
+            Thread.Sleep(1000);
+
+            DateTime d = DateTime.Now;
+
+            string date = d.Year + "_" + d.Month + "_" + d.Day + "_" + d.Hour;
+            UInt64 i = 0;
+
+            dats.date = date;
+
+        oupsi:
+
+            if (!Directory.Exists(to + date))
+            {
+                Directory.CreateDirectory(to + date);
+
+                Backups.Enqueue(to + date);
+            }
+            else
+            {
+                i++;
+                date = d.Year + "_" + d.Month + "_" + d.Day + "_" + d.Hour + "_" + i;
+                goto oupsi;
+            }
+
+            byte[] block = new byte[8192];
+
+            FileInfo fii = new FileInfo(from);
+
+            int last = 0;
+            UInt64 lastCount = 0;
+            UInt64 errors = 0;
+
+            for (UInt64 i2 = 0; i2 < (UInt64)files.Length; i2++)
+            {
+                string s = files[i2];
+
+                int a = (DateTime.Now - d).Seconds;
+
+                if (a > last)
+                {
+                    last = a;
+                    try
+                    {
+                        TitleWrite($"Transfer: {i2}/{files.Length} {i2 / (UInt64)a}/s {((UInt64)files.Length - i2) / (i2 / (UInt64)a)}s");
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+
+                }
+
+
+                try
+                {
+                    FileStream f = File.OpenRead(s);
+
+                    FileInfo fii2 = new FileInfo(s);
+
+                    string fpath = fii2.DirectoryName.Substring(fii.DirectoryName.Length);
+
+
+                    if (!Directory.Exists(to + date + "/" + fpath))
+                        Directory.CreateDirectory(to + date + "/" + fpath);
+
+                    FileStream f2 = File.OpenWrite(to + date + "/" + fpath + "/" + fii2.Name);
+
+                    int l = f.Read(block, 0, block.Length);
+
+                    while (l > 0)
+                    {
+                        f2.Write(block, 0, l);
+                        l = f.Read(block, 0, block.Length);
+                    }
+
+                    f.Close();
+                    f2.Close();
+
+                }
+                catch (Exception)
+                {
+                    errors++;
+                }
+            }
+
+            dats.error = "errors: " + errors.ToString();
+            TitleWrite("Backuper powered by MCSharp.fr");
+        }
+
+    }
+}
---
+++ Backuper/Config.cs
@@ -0,0 +1,134 @@
+﻿using SharpYaml.Serialization;
+using System;
+using System.IO;
+
+namespace Backuper
+{
+    enum BackupOccurences
+    {
+        Hourly,
+        Dayly,
+        Monthly
+    }
+    class BackupQuerry
+    {
+        public BackupOccurences Occurences = BackupOccurences.Dayly;
+        public float OccurenceFactor = 1;
+        public int BackupCountToKeep = 3;
+        public string FolderToBackup = "./ToBackup";
+        public string BackupFolder = "./Backups";
+        public string[] Ignore = null;
+        public string date = "6:00";
+
+        public override string ToString()
+        {
+            string res = "";
+
+            res += $"Occurences: {Occurences}\n";
+            res += $"OccurenceFactor: {OccurenceFactor}\n";
+            res += $"BackupCountToKeep: {BackupCountToKeep}\n";
+            res += $"FolderToSave: {FolderToBackup}\n";
+            res += $"BackupFolder: {BackupFolder}\n";
+            res += $"StartingDate" +
+                $": {date}\n";
+            res += $"Ingore:\n";
+
+            foreach (string s in Ignore)
+                res += $" {s}\n";
+
+            return res;
+        }
+
+    }
+
+    class Config
+    {
+        public BackupQuerry[] Querries;
+
+        const string BaseConfig = @"#
+#
+#  ____             _                             _____             __ _       
+# |  _ \           | |                           / ____|           / _(_)      
+# | |_) | __ _  ___| | ___   _ _ __   ___ _ __  | |     ___  _ __ | |_ _  __ _ 
+# |  _ < / _` |/ __| |/ / | | | '_ \ / _ \ '__| | |    / _ \| '_ \|  _| |/ _` |
+# | |_) | (_| | (__|   <| |_| | |_) |  __/ |    | |___| (_) | | | | | | | (_| |
+# |____/ \__,_|\___|_|\_\\__,_| .__/ \___|_|     \_____\___/|_| |_|_| |_|\__, |
+#                             | |                                         __/ |
+#                             |_|                                        |___/    
+#
+#This is the Backuper config file, feel free to edit it as you want
+#You can use # to write comments in the config file
+
+#Each Backup querry is an individual backup request, and will be independant form other querry, you can add as many as you want
+Querries:
+
+#Example: this will do a backup every two days at 16:42, it will keep last 3 Backups of /root/42
+
+ - #Enum: Hourly,Dayly,Monthly
+   Occurences:         Dayly
+
+   #Multiplier for the above enum
+   OccurenceFactor:    2.0
+
+   #Starting date
+   date:               16:42
+
+   #Number of Backup to keep
+   BackupCountToKeep:  3
+
+   #Folder or file to backup
+   FolderToBackup:     /root/42
+
+   #Destination Folder of the Backups
+   BackupFolder:       /root/Backups
+
+   #Files or folders to ignore, all path are local path from the base folder
+   Ignore:
+
+    #exclude folder 42 from the backup
+    - 42/
+    #exclude file A_file.txt from the backup
+    - A_file.txt
+
+";
+
+        public Config()
+        {
+            if (!File.Exists(Backuper.CurrentFolder + "Config.yml"))
+            {
+                File.WriteAllText(Backuper.CurrentFolder + "Config.yml", BaseConfig);
+
+                Console.WriteLine($"Config File was not found at {Backuper.CurrentFolder + "Config.yml"}, Created a new one\nPress enter to continue");
+
+
+                Console.ReadLine();
+
+            }
+
+            string config = File.ReadAllText(Backuper.CurrentFolder + "Config.yml");
+
+            try
+            {
+                Serializer s = new Serializer();
+                s.DeserializeInto<Config>(config, this);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                Console.WriteLine($"Config File is corrupted at {Backuper.CurrentFolder + "Config.yml"}\nPress enter to create a default one");
+
+                Console.ReadLine();
+
+                File.WriteAllText(Backuper.CurrentFolder + "Config.yml", BaseConfig);
+
+
+
+            }
+
+
+            Console.WriteLine("Successfully loaded Config file: \n" + config);
+
+
+        }
+    }
+}
